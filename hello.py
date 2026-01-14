86492df27f6294d89896312aa3065b23e1575281
DevLakshan <tl6129393@gmail.com>
2026-01-15 01:49:57 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,8 +2,9 @@
 # A simple script to print a greeting message
 
 def main():
-    print("Hello! My name is Tharindu Lakshan (DevLakshan).")
+    print("Hello! My name is Tharindu Lakshan Thilakarathna.")
     print("Welcome to my COE3200 assignment repository.")
 
 if __name__ == "__main__":
+
     main()
